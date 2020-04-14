761e19a4bdb0a9fb1dbde19aacf019be3649393b
Jungea <jungea2241@gmail.com>
2020-04-14 20:19:07 +0900

    QR코드 스캐너 구현

---
+++ app/src/main/java/net/skhu/qrcheck/ScanQRActivity.java
@@ -0,0 +1,37 @@
+package net.skhu.qrcheck;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ScanQRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr);
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "출석 취소", Toast.LENGTH_LONG).show();
+                // todo
+            } else {
+                Toast.makeText(this, result.getContents().toString(), Toast.LENGTH_LONG).show();
+                // todo
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
