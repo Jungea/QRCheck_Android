9f25e9ad3f763e8cf11dcdf99bc5f5b9ca96e2a5
Jungea <jungea2241@gmail.com>
2020-04-17 01:00:04 +0900

    코드 정리

--- app/src/main/java/net/skhu/qrcheck/RetrofitAPI.java
+++ app/src/main/java/net/skhu/qrcheck/RetrofitAPI.java
@@ -12,9 +12,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface RetrofitAPI {
-    // GET/POST/DELETE/PUT 메소드들을 인터페이스에 구현하여 사용할 수 있다.
-//    @GET("api/registration/{sid}/{cid}")
-//    Call<List<Contributor>> contributors(@Path("sid") int sid,  @Path("cid") int cid);
     @GET("api/student/{id}")
     Call<Student> student(@Path("id") int id);
 
@@ -29,13 +26,4 @@ public interface RetrofitAPI {
 
     @GET("api/check/{courId}/{stuId}")
     Call<Message> checkQR(@Path("courId") int courId, @Path("stuId") int stuId);
-
-
-
-//    @GET("/repos/{owner}/{repo}/contributors")
-//        // JSON Array를 리턴하므로 List<>가 되었다
-//    Call<List<Contributor>> contributors(
-//            // param 값으로 들어가는 것들이다
-//            @Path("owner") String owner,
-//            @Path("repo") String repo);
 }
